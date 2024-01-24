804a16c291674562bf0b8dbb986ba1f35e23c846
wavezboy <sanusifatah16@gmail.com>
2024-01-24 01:09:57 +0100

    ;

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import LastOrder from "./components/LastOrder";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col justify-between">
+    <div className="flex min-h-screen flex-col justify-between  bg-[#F7F8FA]">
       <div className="mx-auto mt-20">
         <LastOrder />
       </div>
