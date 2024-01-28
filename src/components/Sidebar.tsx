d0944fd957e0efbe477c9fcfe6607f93408a8cde
wavezboy <sanusifatah16@gmail.com>
2024-01-28 16:31:03 +0100

    sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   ArrowRightIcon,
   BoxIcon,
@@ -18,128 +18,144 @@ export default function Sidebar() {
   const [select, setSelect] = useState(1);
   const [sidebarVisibility, setSidebarVisibility] = useState(false);
   const ref = useOutsideClick(() => setSidebarVisibility(false));
+
+  useEffect(() => {
+    if (sidebarVisibility) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "scroll";
+    }
+  }, [sidebarVisibility]);
+
   return (
-    <div
-      ref={ref}
-      className={`${
-        sidebarVisibility === true ? "left-0" : "-left-[70px]"
-      } fixed sm:left-0 top-0 z-20 transition-all duration-500 `}
-    >
-      <div className="h-screen w-[80px] py-[20px]  bg-[#F7F8FA] border-r border-[#E5EAEF] flex flex-col justify-between">
-        <div className="flex  flex-col gap-[32px]  justify-center items-center">
-          <button>
-            <LogoIcon />
-          </button>
-
-          <div className="flex justify-end gap-[24px] w-full">
-            <button
-              onClick={() => {
-                setSelect(1);
-              }}
-            >
-              <CategoryIcon />
+    <>
+      <div
+        className={`fixed h-screen inset-0 w-full backdrop-blur-[2px] z-10 transition-opacity duration-500 ${
+          sidebarVisibility ? "opacity-100 visible" : "opacity-0 invisible"
+        }`}
+      ></div>
+      <div
+        ref={ref}
+        className={`${
+          sidebarVisibility === true ? "left-0 " : "-left-[70px]"
+        } fixed sm:left-0 top-0 z-20 transition-all duration-500 `}
+      >
+        <div className="h-screen w-[80px] py-[20px]  bg-[#F7F8FA] border-r border-[#E5EAEF] flex flex-col justify-between">
+          <div className="flex  flex-col gap-[32px]  justify-center items-center">
+            <button>
+              <LogoIcon />
             </button>
+
+            <div className="flex justify-end gap-[24px] w-full">
+              <button
+                onClick={() => {
+                  setSelect(1);
+                }}
+              >
+                <CategoryIcon />
+              </button>
+              <button
+                onClick={() => {
+                  setSidebarVisibility((prev) => !prev);
+                }}
+              >
+                <SideIcon color={`${select == 1 ? "#0D062D" : "#F7F8FA"}`} />
+              </button>
+            </div>
+
+            <div className="flex justify-end gap-[24px] w-full">
+              <button
+                onClick={() => {
+                  setSelect(2);
+                }}
+              >
+                <TrendUpIcon />
+              </button>
+              <button
+                onClick={() => {
+                  setSidebarVisibility((prev) => !prev);
+                }}
+              >
+                <SideIcon color={`${select == 2 ? "#0D062D" : "#F7F8FA"}`} />
+              </button>
+            </div>
+
+            <div className="flex justify-end gap-[24px] w-full">
+              <button
+                onClick={() => {
+                  setSelect(3);
+                }}
+              >
+                <ProfileIcon />
+              </button>
+              <button
+                onClick={() => {
+                  setSidebarVisibility((prev) => !prev);
+                }}
+              >
+                <SideIcon color={`${select == 3 ? "#0D062D" : "#F7F8FA"}`} />
+              </button>
+            </div>
+
+            <div className="flex justify-end gap-[24px] w-full">
+              <button
+                onClick={() => {
+                  setSelect(4);
+                }}
+              >
+                <BoxIcon />
+              </button>
+              <button
+                onClick={() => {
+                  setSidebarVisibility((prev) => !prev);
+                }}
+              >
+                <SideIcon color={`${select == 4 ? "#0D062D" : "#F7F8FA"}`} />
+              </button>
+            </div>
+
+            <div className="flex justify-end gap-[24px] w-full">
+              <button
+                onClick={() => {
+                  setSelect(5);
+                }}
+              >
+                <DiscountIcon />
+              </button>
+              <button
+                onClick={() => {
+                  setSidebarVisibility((prev) => !prev);
+                }}
+              >
+                <SideIcon color={`${select == 5 ? "#0D062D" : "#F7F8FA"}`} />
+              </button>
+            </div>
+
             <button
               onClick={() => {
-                setSidebarVisibility((prev) => !prev);
+                setSelect(7);
               }}
             >
-              <SideIcon color={`${select == 1 ? "#0D062D" : "#F7F8FA"}`} />
+              <Theme />
             </button>
           </div>
 
-          <div className="flex justify-end gap-[24px] w-full">
-            <button
-              onClick={() => {
-                setSelect(2);
-              }}
-            >
-              <TrendUpIcon />
-            </button>
-            <button
-              onClick={() => {
-                setSidebarVisibility((prev) => !prev);
-              }}
-            >
-              <SideIcon color={`${select == 2 ? "#0D062D" : "#F7F8FA"}`} />
-            </button>
-          </div>
-
-          <div className="flex justify-end gap-[24px] w-full">
-            <button
-              onClick={() => {
-                setSelect(3);
-              }}
-            >
-              <ProfileIcon />
-            </button>
-            <button
-              onClick={() => {
-                setSidebarVisibility((prev) => !prev);
-              }}
-            >
-              <SideIcon color={`${select == 3 ? "#0D062D" : "#F7F8FA"}`} />
-            </button>
-          </div>
-
-          <div className="flex justify-end gap-[24px] w-full">
-            <button
-              onClick={() => {
-                setSelect(4);
-              }}
-            >
-              <BoxIcon />
-            </button>
-            <button
-              onClick={() => {
-                setSidebarVisibility((prev) => !prev);
-              }}
-            >
-              <SideIcon color={`${select == 4 ? "#0D062D" : "#F7F8FA"}`} />
-            </button>
-          </div>
-
-          <div className="flex justify-end gap-[24px] w-full">
-            <button
-              onClick={() => {
-                setSelect(5);
-              }}
-            >
-              <DiscountIcon />
-            </button>
-            <button
-              onClick={() => {
-                setSidebarVisibility((prev) => !prev);
-              }}
-            >
-              <SideIcon color={`${select == 5 ? "#0D062D" : "#F7F8FA"}`} />
-            </button>
-          </div>
-
-          <button
-            onClick={() => {
-              setSelect(7);
-            }}
-          >
-            <Theme />
-          </button>
-        </div>
-
-        <div
-          className="flex flex-col gap-[32px] items-center
+          <div
+            className="flex flex-col gap-[32px] items-center
         justify-center "
-        >
-          <button>
-            <ArrowRightIcon />
-          </button>
-          <button>
-            <SettingIcon />
-          </button>
-          <button>
-            <LogoutIcon />
-          </button>
+          >
+            <button>
+              <ArrowRightIcon />
+            </button>
+            <button>
+              <SettingIcon />
+            </button>
+            <button>
+              <LogoutIcon />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
